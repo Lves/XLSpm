5bb9db7a263408057aa86c27c59c81601c05c1eb
xingle.li <lveslxl@gmail.com>
2025-12-23 18:15:58 +0800

    add files

---
+++ Sources/XLSpm/XLTools.swift
@@ -0,0 +1,14 @@
+//
+//  XLTools.swift
+//  XLSpm
+//
+//  Created by xingle on 2025/12/23.
+//
+
+public class XLTools {
+    public static var name: String = "name001"
+    
+    public class func getAge() -> Int {
+        return 1
+    }
+}
